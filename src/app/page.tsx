f38586ae49691fc9e1dc6663ada477ba1d05b738
ZeyadZaher03 <zeyadzaher02@gmail.com>
2024-02-23 00:56:57 +0200

    fix responsiveness in navigation menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,23 +15,7 @@ import homePageInfo from "./pageInfo/homePageInfo.json";
 import { BlogSize, ServiceItemTypes } from "./types/types";
 import { ToastContainer } from "react-toastify";
 
-interface IHomePageData {
-  hero?: any;
-  services?: {
-    selectedServices: string[];
-    header: string;
-    subtitle: string;
-    buttonText: string;
-  } | null;
-  faq?: any;
-  aboutUs?: any;
-  info?: any;
-  partnerUp?: any;
-  blogs?: any;
-}
-
-export default function Home() {
-  // Access the data sections
+export default function HomePage() {
   const heroSection = homePageInfo?.hero;
   const serviceSection = homePageInfo?.services;
   const faqSection = homePageInfo?.faq;
@@ -41,74 +25,21 @@ export default function Home() {
   const blogsSection = homePageInfo?.blogs;
 
   return (
-    <>
-      <ToastContainer />
+    <div>
       <Nav currentPathName={"/"} />
-      <main>
+      {/* <main>
         {heroSection && (
           <HeroSection
-            imgSrc={heroSection?.imgSrc}
-            imgPos={heroSection?.overlay?.imgPos}
+            imgSrc={heroSection.imgSrc}
+            imgPos={heroSection.overlay?.imgPos}
             overlay={heroSection.overlay}
-            header={heroSection?.info?.header}
-            subHeader={heroSection?.info?.subtitle}
-            body={heroSection?.info?.body}
+            header={heroSection.info?.header}
+            subHeader={heroSection.info.subtitle}
+            body={heroSection.info.body}
           />
         )}
-        {serviceSection && (
-          <ServicesSection
-            selectedServices={serviceSection?.selectedServices}
-            header={serviceSection?.header}
-            subtitle={serviceSection?.subtitle}
-            buttonText={serviceSection?.buttonText}
-            type={ServiceItemTypes.typeOne}
-          />
-        )}
-        {faqSection && (
-          <FAQSection
-            title={faqSection?.header}
-            buttonText={faqSection?.buttonText}
-            selectedFaqs={faqSection?.selectedFaqs}
-          />
-        )}
-        {aboutUsSection && (
-          <AboutUsSection
-            title={aboutUsSection?.title}
-            subTitle={aboutUsSection?.subtitle}
-            imgPos={aboutUsSection?.overlay?.imgPos}
-            imgSrc={aboutUsSection?.imgSrc}
-          />
-        )}
-        {infoSection && <InfoSection info={infoSection} />}
-        {partnerUp && (
-          <BenefitsSection
-            title={partnerUp?.title}
-            benefitsOne={partnerUp?.benefitsOne}
-            benefitsTwo={partnerUp?.benefitsTwo}
-            benefitsThree={partnerUp?.benefitsThree}
-          />
-        )}
-        aaa
-        {/* <Testimonials title={"TESTIMONIALS"} subTitle={"What Our Clients Say"} /> */}
-        {blogsSection && (
-          <BlogSection
-            cols={3}
-            size={BlogSize.MEDIUM}
-            title={blogsSection?.header}
-            subTitle={blogsSection?.subtitle}
-            selectedBlogs={blogsSection?.selectedBlogs}
-            buttonText={blogsSection.buttonText}
-          />
-        )}
-        <CallToActionSection
-          title={"Learn more about our services"}
-          video={"https://dialpros.com/wp-content/uploads/2022/12/video.mp4"}
-          subTitle={"Schedule a free consultation with us"}
-          href={"#"}
-          buttonText={"Schedule A Call"}
-        />
-      </main>
-      <Footer />
-    </>
+      </main> */}
+      {/* <Footer /> */}
+    </div>
   );
 }
---
+++ src/components/Nav/MobileNavigation/MobileNav.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import classnames from "classnames";
+import { HamburgerMenu } from "./HamburgerMenu";
+import { MobileNavigationLink } from "./MobileNavigationLink";
+
+interface MobileNavigationProps {
+  links: { title: string; href: string }[];
+  currentPathName: string;
+}
+
+export const MobileNavigation = ({
+  links,
+  currentPathName,
+}: MobileNavigationProps) => {
+  const [menuOpened, setMenuOpened] = useState(false);
+
+  const burgerMenuClasses = classnames("navigation__hamburger-wrapper", {
+    "navigation__hamburger-wrapper--active": menuOpened,
+  });
+
+  const onClickOnHamburgerMenu = () => setMenuOpened((p) => !p);
+
+  useEffect(() => {
+    document.body.style.overflow = menuOpened ? "hidden" : "";
+    document.body.style.height = menuOpened ? "100vh" : "";
+  }, [menuOpened]);
+
+  return (
+    <>
+      <HamburgerMenu onClick={onClickOnHamburgerMenu} />
+      <div className={burgerMenuClasses}>
+        <span className="nav-hamburger__header">Menu</span>
+        <ul className="nav-hamburger__list">
+          {links.map(({ title, href }) => (
+            <MobileNavigationLink
+              currentPathName={currentPathName}
+              href={href}
+              title={title}
+            />
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+};
